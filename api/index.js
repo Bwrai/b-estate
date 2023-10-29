ae643d950b9eed501cc27bea6bcb517dabf372af
Manveer <manveer.bwrai@gmail.com>
2023-10-29 13:20:56 +0530

    Creating api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotEnv from 'dotenv';
+import userRouter from './routes/userRoutes.js'
+
 dotEnv.config()
 
 const app = express();
@@ -9,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 async function mongoConnection() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb://127.0.0.1:27017/myapp');
         console.log("DATABASE CONNECTED")
     } catch (err) {
         console.error("MONGO CONNECTION FAILED", err)
@@ -20,3 +22,5 @@ mongoConnection()
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
+
+app.use('/api/user', userRouter)
