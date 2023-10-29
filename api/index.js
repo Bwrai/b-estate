395b97dae8d45acfab2aa3c40f24bd125e68cc4b
Manveer <manveer.bwrai@gmail.com>
2023-10-29 09:41:43 +0530

    Creating mongooDB connection/configuring .env file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotEnv from 'dotenv';
+dotEnv.config()
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("DATABASE CONNECTED")
+    })
+    .catch((err) => {
+        console.error("MONGO CONNECTION FAILED:",err);
+    })
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
