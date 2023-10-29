78ba538b1b387ae4e70b4a49019719bd5317f170
Manveer <manveer.bwrai@gmail.com>
2023-10-29 12:03:28 +0530

    Refactoring mongoose connection with async

--- api/index.js
+++ api/index.js
@@ -7,13 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
+async function mongoConnection() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DATABASE CONNECTED")
-    })
-    .catch((err) => {
-        console.error("MONGO CONNECTION FAILED:",err);
-    })
+    } catch (err) {
+        console.error("MONGO CONNECTION FAILED", err)
+    }
+}
+mongoConnection()
+
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 })
