c5a504fb3e6d8c32b20305a263877833a00d9a53
Manveer <manveer.bwrai@gmail.com>
2023-11-24 15:13:33 +0530

    refactoring misplaced bracket

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -32,7 +32,7 @@ export const updateListing = async (req, res, next) => {
         return next(errorHandler(404, "Listing not found"))
     }
     if (req.user.id !== listing.userRef) {
-        return next(errorHandler(401), "You can only update your own listings!");
+        return next(errorHandler(401, "You can only update your own listings!"));
     }
     try {
         const updatedListing = await Listing.findByIdAndUpdate(
