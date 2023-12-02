88251771d15324b6cf1fd14217e0281958822257
Manveer <manveer.bwrai@gmail.com>
2023-12-02 14:13:43 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-10 px-5 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-600">About Us</h1>
+      <p className="mb-3 text-slate-700">
+        BwraiEstate is a leading real estate agency that specializes in helping
+        clients to buy, sell and rent properties. At BwraiEstate, we understand
+        that buying, selling, or investing in real estate is more than just a
+        transaction; it's a significant life decision. That's why our team of
+        dedicated professionals is committed to providing exceptional service,
+        personalized attention, and expert guidance to make your real estate
+        journey a seamless and rewarding experience.
+      </p>
+      <h1 className="text-3xl font-bold mb-4 text-slate-600">Our Mission</h1>
+      <p className="mb-3 text-slate-700">
+        Our mission is to empower individuals and families to achieve their real
+        estate goals by providing the highest level of service, market
+        expertise, and integrity. Whether you're a first-time homebuyer, a
+        seasoned investor, or looking to sell your property, we are here to
+        guide you every step of the way.
+      </p>
+      <h1 className="text-3xl font-bold mb-4 text-slate-600">Contact Us</h1>
+      <p className="mb-3 text-slate-700">
+        Contact Us Ready to embark on your real estate journey with BwraiEstate?
+        Whether you're buying, selling, or investing, we're here to help.
+        Contact us today to schedule a consultation and discover the difference
+        personalized service can make <span className="underline text-blue-700">bestate@gmail.com</span>  Thank you for
+        considering BwraiEstate as your trusted real estate
+        partner. We look forward to assisting you in achieving your real estate
+        goals!
+      </p>
+    </div>
+  );
 }
